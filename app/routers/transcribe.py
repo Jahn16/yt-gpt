e0a5cead73f5d1be7edbaf6e47db1f82e16b24cf
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-03-17 00:02:39 -0300

    🚧 Integrate with youtube client

--- app/routers/transcribe.py
+++ app/routers/transcribe.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
+from app.providers.youtube import YoutubeClient
+
 router = APIRouter(prefix="/transcribe")
 
 
 @router.get("/")
-def transcribe(youtube_url: str) -> dict[str, str]:
-    return {"youtube_url": youtube_url}
+def transcribe(youtube_url: str) -> str:
+    transcription = YoutubeClient.get_transcript(youtube_url)
+    return transcription
