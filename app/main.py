723005f6f8bd3b806d42afff1ebb1c4ae47ee9e2
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-03-16 23:09:32 -0300

    🔊 Add structlog support

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, FastAPI
 
 from app.routers import transcribe
+from app.utils.logger import setup_logging
+
+setup_logging()
 
 app = FastAPI(title="YT-GPT", version="0.1.0")
 
