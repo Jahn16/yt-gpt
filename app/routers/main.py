8d231bdc14c5e7d5fff2940f8e031931aaaefc80
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-04-23 19:29:51 -0300

    ✨ Split gpt and transcribe functions

--- app/routers/main.py
+++ app/routers/main.py
@@ -5,14 +5,19 @@ from app.deps import get_settings
 from app.providers.openai import OpenAIClient
 from app.providers.youtube import YoutubeClient
 
-router = APIRouter(prefix="/transcribe")
+router = APIRouter()
 
 
-@router.get("/")
-async def transcribe(
-    youtube_url: str, prompt: str, settings: Settings = Depends(get_settings)
-) -> str:
+@router.get("/transcribe")
+async def transcribe(youtube_url: str) -> str:
     transcription = YoutubeClient.get_transcript(youtube_url)
+    return transcription
+
+
+@router.get("/gpt")
+async def gpt(
+    transcription: str, prompt: str, settings: Settings = Depends(get_settings)
+) -> str:
     openai_client = OpenAIClient(settings)
     result = await openai_client.chat(prompt, transcription)
 
