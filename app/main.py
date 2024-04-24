ca42f9289c20d6fb9f1d7e30a982c23cb8ca07fb
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-04-24 07:55:01 -0300

    🐛 Fix include router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI
 
-from app.routers import transcribe
+from app.routers import main
 from app.utils.logger import setup_logging
 
 setup_logging()
@@ -9,6 +9,6 @@ app = FastAPI(title="YT-GPT", version="0.1.0")
 
 
 api_router = APIRouter(prefix="/api/v1")
-api_router.include_router(transcribe.router)
+api_router.include_router(main.router)
 
 app.include_router(api_router)
