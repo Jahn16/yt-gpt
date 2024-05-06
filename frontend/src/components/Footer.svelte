e2f586a9a5bf575d001fe040d249f33100cdf532
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-05-06 20:31:20 -0300

    🐛 Fix a bug that made the footer block buttons

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="container fixed-bottom">
+<div class="container static-bottom">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-4 d-flex align-items-center">
       <span class="mb-3 mb-md-0 text-muted">YT-GPT v1.2.1</span>
