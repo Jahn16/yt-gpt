0425ffaddb064ce931bd52501af1fe1c68836596
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-05-23 21:15:49 -0300

    🔖 Change version to 1.3.0

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -1,11 +1,15 @@
 <div class="container static-bottom">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <span class="mb-3 mb-md-0 text-muted">YT-GPT v1.2.1</span>
-    </div>
+	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+		<div class="col-md-4 d-flex align-items-center">
+			<span class="mb-3 mb-md-0 text-muted">YT-GPT v1.3.0</span>
+		</div>
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-muted" href="https://github.com/Jahn16/yt-gpt" target="_blank"><i class="bi bi-github"></i></a></li>
-    </ul>
-  </footer>
+		<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+			<li class="ms-3">
+				<a class="text-muted" href="https://github.com/Jahn16/yt-gpt" target="_blank"
+					><i class="bi bi-github"></i></a
+				>
+			</li>
+		</ul>
+	</footer>
 </div>
