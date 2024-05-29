b1828e0efb6986eedffc84b8c6106c0387979038
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-05-29 20:40:00 -0300

    🔖 Bump version to 1.4.0

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <div class="container static-bottom">
 	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 		<div class="col-md-4 d-flex align-items-center">
-			<span class="mb-3 mb-md-0 text-muted">YT-GPT v1.3.0</span>
+			<span class="mb-3 mb-md-0 text-muted">YT-GPT v1.4.0</span>
 		</div>
 
 		<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
